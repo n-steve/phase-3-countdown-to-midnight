f8ac87f8ae5226a3b2e1dc95a1b7263b4239defa
steve <steve.nguyen2013@gmail.com>
2022-07-27 14:10:56 -0700

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(int)
+    x = 10
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        sleep 1
+        x -= 1
+    end
+        return "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(int)
+    x = 10
+    while x > 0 
+        sleep 5
+        x -= 1
+    end
 end
